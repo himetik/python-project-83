8f9a6818e815535915eeebcd87bef925e8a3c441
himetik <himetik@tutamail.com>
2024-10-11 13:05:57 +0300

    Configuration added

---
+++ page_analyzer/config.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY")
+MINCONN = os.getenv("MINCONN", "2")
+MAXCONN = os.getenv("MAXCONN", "3")
