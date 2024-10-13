bda26951f2cdcdd404f7a07f4673c031205209a5
himetik <himetik@tutamail.com>
2024-10-13 17:20:44 +0300

    Add function docstrings in utils module

--- page_analyzer/utils.py
+++ page_analyzer/utils.py
@@ -9,6 +9,9 @@ def normalize_url(url: str) -> str:
 
 
 def get_accessibility_content(response: Response) -> dict:
+    """
+    Parses the response HTML and extracts the status code, H1 tag, title, and description.
+    """
     soup = BeautifulSoup(response.text, "html.parser")
 
     h1 = soup.find("h1").text if soup.find("h1") else ""
